54b3884bb26c558f151f4902c65e35159acba681
emreonal11 <73316684+emreonal11@users.noreply.github.com>
2022-09-01 13:39:58 +0200

    Rename overfit_test.sh to test.sh

--- overfit_test.sh
+++
@@ -1,2 +0,0 @@
-set -ex
-python ./test.py --dataroot ./datasets/overfit --checkpoints_dir ./checkpoints7  --name last --img_height 324 --img_width 576 --preprocess none --load_size 576 --input_nc 3 --output_nc 1 --direction AtoB --lambda_recon 120 --lambda_smooth 200 --lr 0.0005 --num_threads 0 --gpu_ids 0 --batch_size 1  --stn_type unet --stn_bilateral_alpha 1 --init_type normal --netG resnet_6blocks --phase test --show_by_ratio --show_ratio 1
